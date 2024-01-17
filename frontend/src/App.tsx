5be94621ab1ac203b04905568ce5ca79db64a5b6
Rudro Debnath <rudro.cse5.bu@gmail.com>
2024-01-18 04:06:38 +0600

    Home Page css added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,16 +4,25 @@ import {
   Routes,
   Navigate,
 } from "react-router-dom";
-//import Layout from "./layouts/Layouts";
-import Header from "./components/Header";
+import Layout from "./layouts/Layouts";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Header></Header>} />
-        {/* <Route path="/search" element={<>Search Page</>} />
-        <Route path="*" element={<Navigate to="/" />} /> */}
+        <Route path="/" 
+        element={
+          <Layout>
+            <p>Home Page</p>
+          </Layout>
+        } />
+        <Route path="/search" 
+        element={
+          <Layout>
+            <p>Search Page</p>
+          </Layout>
+        } />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   )
