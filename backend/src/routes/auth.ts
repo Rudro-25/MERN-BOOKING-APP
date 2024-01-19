3d6f56003d1ebad3ed20b602263453cb63342ac0
Rudro Debnath <rudro.cse5.bu@gmail.com>
2024-01-20 03:43:17 +0600

    Register page with backend and some part of home

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -3,6 +3,8 @@ import { check, validationResult } from "express-validator";
 import User from "../models/user";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { verify } from "crypto";
+import verifyToken from "../middleware/auth";
 
 const router = express.Router();
 
@@ -49,6 +51,10 @@ router.post("/login", [
         res.status(500).json({ message: "Something went wrong" });
     }
     
+});
+
+router.get("/validate-token", verifyToken, (req: Request, res: Response) => {
+    res.status(200).send({userId: req.userId})
 });
 
 export default router;
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,4 +1,8 @@
 import { useForm } from "react-hook-form";
+import { useMutation } from "react-query";
+import * as apiClient from '../api-client';
+import { useAppContext } from "../contexts/AppContext";
+import { useNavigate } from "react-router-dom";
 
 export type RegisterFormData = {
     firstName: string,
@@ -9,6 +13,8 @@ export type RegisterFormData = {
 }
 
 const Register = () => {
+    const navigate = useNavigate();
+    const { showToast } = useAppContext();
     const { 
         register,
         watch, 
@@ -16,8 +22,18 @@ const Register = () => {
         formState : { errors }, 
     } = useForm<RegisterFormData>();
 
+    const mutation = useMutation(apiClient.register, {
+        onSuccess: () => {
+            showToast({ message: "Registration Success!", type: "SUCCESS" });
+            navigate("/");
+        },
+        onError: (error: Error)=> {
+            showToast({ message: error.message, type: "ERROR" });
+        }
+    });
+
     const onSubmit = handleSubmit((data) => {
-        console.log(data);
+        mutation.mutate(data);
     })
  
     return (
