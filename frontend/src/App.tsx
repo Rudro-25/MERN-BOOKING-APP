bc4f5863226a0dd1ce4b80c6b03d05f97545f358
Rudro Debnath <rudro.cse5.bu@gmail.com>
2024-01-27 14:33:55 +0600

    add-hotel page created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,37 +7,53 @@ import {
 import Layout from "./layouts/Layouts";
 import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
+import { useAppContext } from "./contexts/AppContext";
+import AddHotel from "./pages/AddHotel";
 
 const App = () => {
+  const {isLoggedIn} = useAppContext();
   return (
     <Router>
       <Routes>
-        <Route path="/" 
-        element={
-          <Layout>
-            <p>Home Page</p>
-          </Layout>
-        } />
-        <Route path="/search" 
-        element={
-          <Layout>
-            <p>Search Page</p>
-          </Layout>
-        } />
-        <Route 
-          path="/register" 
+        <Route path="/"
           element={
-          <Layout>
-            <Register/>
-          </Layout>
-          }/>
-        <Route 
-          path="/sign-in" 
+            <Layout>
+              <p>Home Page</p>
+            </Layout>
+          } />
+        <Route path="/search"
           element={
-          <Layout>
-            <SignIn/>
-          </Layout>
-          }/>
+            <Layout>
+              <p>Search Page</p>
+            </Layout>
+          } />
+        <Route
+          path="/register"
+          element={
+            <Layout>
+              <Register />
+            </Layout>
+          } />
+        <Route
+          path="/sign-in"
+          element={
+            <Layout>
+              <SignIn />
+            </Layout>
+          }
+        />
+        {isLoggedIn && (
+          <>
+            <Route 
+              path="/add-hotel"  
+                element={
+                  <Layout>
+                    <AddHotel />
+                  </Layout>
+                }
+            />
+          </>
+        )}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
